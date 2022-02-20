2d7e9dab6a580c2c0cade9261452bf08c97512b2
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2022-02-20 15:35:09 +0300

    Fixed up

--- opcache-reset.php
+++ opcache-reset.php
@@ -3,7 +3,7 @@
  * Plugin Name: OPcache Reset
  * Plugin URI: http://wordpress.org/plugins/opcache-reset/
  * Description: Automatic reset of OPcache
- * Version: 2.1.6
+ * Version: 2.1.7
  * Author: Danila Vershinin
  * Author URI: https://www.getpagespeed.com/
  * License: GPL2
